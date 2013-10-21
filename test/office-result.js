5cae59ee72115ebb7318a05524ed3505cf7825ba
Julien <julien.balian@supinfo.com>
2013-10-21 12:34:17 +0200

    test docx.

--- test/office-result.js
+++ test/office-result.js
@@ -23,5 +23,18 @@ describe('Test office results', function() {
         .should.include('Game Design');
       done();
     });
+
+
+    office(__dirname+"/samples/test2.docx", document, function(err, document) {
+      if(err) {
+        throw err;
+      }
+      document.should.have.property('metadatas');
+      //document.should.have.property('binary_document_type', "office-html");
+      document.metadatas.should.have.property('html');
+      document.metadatas.html
+        .should.include('purpose of this document');
+      done();
+    });
   });
 });
