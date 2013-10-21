c864f7b55820b7320cf89a67cb893d6a6f2e4bee
Neamar <neamar@neamar.fr>
2013-10-21 16:09:40 +0200

    Remove old test

--- test/office.js
+++ test/office.js
@@ -6,30 +6,12 @@ var office = require('../lib/hydrater-office');
 
 
 describe('Test office results', function() {
-  it('returns the correct informations for pptx', function(done) {
-    var document = {
-      metadatas: {}
-    };
-
-    office(__dirname + "/samples/test.pptx", document, function(err, document) {
-      if(err) {
-        throw err;
-      }
-
-      document.should.have.property('metadatas');
-      document.should.have.property('binary_document_type', "document");
-      document.metadatas.should.have.property('html');
-      document.metadatas.html.should.include('Game Design');
-      done();
-    });
-  });
-
   it('returns the correct informations for docx', function(done) {
     var document = {
       metadatas: {}
     };
 
-    office(__dirname + "/samples/test.docx", document, function(err, document) {
+    office(__dirname + "/samples/test1.docx", document, function(err, document) {
       if(err) {
         throw err;
       }
@@ -38,6 +20,24 @@ describe('Test office results', function() {
       document.should.have.property('binary_document_type', "document");
       document.metadatas.should.have.property('html');
       document.metadatas.html.should.include('purpose of this document');
+      done();
+    });
+  });
+
+  it('returns the correct informations for pptx', function(done) {
+    var document = {
+      metadatas: {}
+    };
+
+    office(__dirname + "/samples/test2.pptx", document, function(err, document) {
+      if(err) {
+        throw err;
+      }
+
+      document.should.have.property('metadatas');
+      document.should.have.property('binary_document_type', "document");
+      document.metadatas.should.have.property('html');
+      document.metadatas.html.should.include('Game Design');
       done();
     });
   });
