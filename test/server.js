fc300acacf700aac72892122f7792665cf75d358
Neamar <neamar@neamar.fr>
2014-02-03 17:55:25 +0100

    Improved tests

--- test/server.js
+++ test/server.js
@@ -12,7 +12,14 @@ var config = require('../config/configuration.js');
 
 
 describe('Test office results', function() {
-  it.only('should call the pdf hydrater before sending results', function(done) {
+  before(function() {
+    officeHydrater.listen(1339);
+  });
+  after(function(done) {
+    officeHydrater.close(done);
+  });
+
+  it('should call the pdf hydrater before sending results', function(done) {
     // Update our pdf hydrater
     officeHydrater.listen(1339);
     config.pdf_hydrater_url = 'http://localhost:1337/hydrate';
@@ -28,13 +35,6 @@ describe('Test office results', function() {
           return done(err);
         }
 
-        // Check we have a valid PDF
-        try {
-          res.body.should.contain("Transparency/CS/DeviceRGB");
-        } catch(e) {
-          return done(e);
-        }
-        
         var payload = {
           url: req.params.callback,
           json: {
@@ -64,7 +64,9 @@ describe('Test office results', function() {
       res.send(202);
       next();
 
-      officeHydrater.close(done);
+      core.close(function() {
+        pdfHydrater.close(done);
+      });
     });
 
     core.listen(1338);
@@ -85,6 +87,66 @@ describe('Test office results', function() {
           throw err;
         }
       });
+  });
 
+  it('should transform to PDF', function(done) {
+    // Update our pdf hydrater
+    config.pdf_hydrater_url = 'http://localhost:1337/hydrate';
+    config.office_hydrater_url = 'http://localhost:1339';
+
+    var pdfHydrater = restify.createServer();
+    pdfHydrater.use(restify.queryParser());
+    pdfHydrater.use(restify.bodyParser());
+
+    pdfHydrater.post('/hydrate', function(req, res, next) {
+      baseRequest.get(req.params.file_path, function(err, res) {
+        if(err) {
+          return done(err);
+        }
+
+        // Check we have a valid PDF
+        try {
+          res.body.should.contain("Transparency/CS/DeviceRGB");
+        } catch(e) {
+          return done(e);
+        }
+        
+        core.close(function() {
+          pdfHydrater.close(done);
+        });
+
+        res.send(202);
+        next(err);
+      });
+    });
+    pdfHydrater.listen(1337);
+    
+    // Create fake initial server
+    var core = restify.createServer();
+    core.use(restify.queryParser());
+    core.use(restify.bodyParser());
+    core.get('/document', function(req, res, next) {
+      fs.createReadStream(__dirname + '/samples/text.rtf').pipe(res);
+      next();
+    });
+    core.listen(1338);
+
+    request(officeHydrater)
+      .post("/hydrate")
+      .send({
+        file_path: 'http://localhost:1338/document',
+        callback: 'http://localhost:1338/callback',
+        document: {
+          metadatas: {
+            path: '/samples/text.rtf'
+          }
+        }
+      })
+      .expect(202)
+      .end(function(err) {
+        if(err) {
+          throw err;
+        }
+      });
   });
 });
