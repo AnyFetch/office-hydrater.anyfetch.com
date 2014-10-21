f9c749fd607d439e4e5de536bd29b8065aebf9c2
Amoki <hugoduroux@gmail.com>
2014-10-21 12:11:36 +0200

    fis tests

--- config/configuration.js
+++ config/configuration.js
@@ -7,10 +7,10 @@ var node_env = process.env.NODE_ENV || "development";
 var defaultPort = 8000;
 
 // URL of pdf hydrater
-var defaultPdfHydraterUrl = "http://pdf.hydrater.anyfetch.com";
+var defaultPdfHydraterUrl = "https://pdf.anyfetch.com";
 
 // URL of office hydrater
-var defaultOfficeHydraterUrl = 'http://office.hydrater.anyfetch.com';
+var defaultOfficeHydraterUrl = 'https://office.anyfetch.com';
 
 // Number of office instance to run simultaneously per cluster
 var defaultConcurrency = 1;
@@ -24,6 +24,6 @@ module.exports = {
   env: node_env,
   port: process.env.PORT || defaultPort,
   concurrency: process.env.OFFICE_CONCURRENCY || defaultConcurrency,
-  pdfHydraterUrl: process.env.PDF_HYDRATER_URL ||defaultPdfHydraterUrl,
+  pdfHydraterUrl: process.env.PDF_HYDRATER_URL || defaultPdfHydraterUrl,
   officeHydraterUrl: process.env.OFFICE_HYDRATER_URL || defaultOfficeHydraterUrl
 };
