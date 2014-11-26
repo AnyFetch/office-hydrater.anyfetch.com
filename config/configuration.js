49b280346df6110e0896d77246c9e4cbfb40c4d0
Amoki <hugoduroux@gmail.com>
2014-11-26 15:00:21 +0100

    update libs

--- config/configuration.js
+++ config/configuration.js
@@ -3,7 +3,7 @@
  */
 
 // node_env can either be "development" or "production"
-var node_env = process.env.NODE_ENV || "development";
+var nodeEnv = process.env.NODE_ENV || "development";
 var defaultPort = 8000;
 
 // URL of pdf hydrater
@@ -15,15 +15,16 @@ var defaultOfficeHydraterUrl = 'https://office.anyfetch.com';
 // Number of office instance to run simultaneously per cluster
 var defaultConcurrency = 1;
 
-if(node_env === "production") {
+if(nodeEnv === "production") {
   defaultPort = 80;
 }
 
 // Exports configuration
 module.exports = {
-  env: node_env,
+  env: nodeEnv,
   port: process.env.PORT || defaultPort,
   concurrency: process.env.OFFICE_CONCURRENCY || defaultConcurrency,
   pdfHydraterUrl: process.env.PDF_HYDRATER_URL || defaultPdfHydraterUrl,
-  officeHydraterUrl: process.env.OFFICE_HYDRATER_URL || defaultOfficeHydraterUrl
+  officeHydraterUrl: process.env.OFFICE_HYDRATER_URL || defaultOfficeHydraterUrl,
+  appName: process.env.APP_NAME || "office-hydrater"
 };
